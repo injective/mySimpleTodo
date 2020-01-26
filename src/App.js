516b70e1382f964a3d92a90b9a28959f7bad9a8b
Sereda Vadim <flash.brovary@gmail.com>
2020-01-26 18:26:58 +0200

    fixed methods, added destructuring, fixed names of variables (#1)
    * fixed methods, added destructuring, fixed names of variables
    
    * minor fixes

--- src/App.js
+++ src/App.js
@@ -10,49 +10,50 @@ class App extends Component {
           list: [],
           inputValue: ''
       }
-
-      this.addHandler = this.addHandler.bind(this);
-      this.changeInputHandler = this.changeInputHandler.bind(this);
     }
 
-    addHandler() {
-        let newObj = {
-            text: this.state.inputValue
-        };
+    onClickAddItem = () => {
+        if(this.state.inputValue.length) {
+            const todoItem = {
+                text: this.state.inputValue
+            };
 
-        console.log(newObj);
-
-        this.setState({
-            list: [...this.state.list, newObj],
-            inputValue: ''
-        });
-
-        console.log(this.state.list);
+            this.setState((state) => ({
+                list: [...state.list, todoItem],
+                inputValue: ''
+            }));
+        }
     };
 
-    changeInputHandler = (e) => {
+    onChangeInput = (e) => {
         this.setState({
             inputValue: e.target.value
         })
     };
 
-
     render() {
-
+        const { list, inputValue } = this.state;
+        const todoItems = list.map((item, id) => {
+            return (
+                <TodoListItem
+                    key={id}
+                    val={item} />
+            );
+        });
 
         return (
           <div className='App'>
               <div className='formWrapper'>
-                  <input className='inputValue' type='text' value={this.state.inputValue} onChange={this.changeInputHandler} />
-                  <button className='btnValue' onClick={this.addHandler}>add</button>
+                  <input
+                      className='inputValue'
+                      type='text'
+                      value={inputValue}
+                      onChange={this.onChangeInput} />
+                  <button className='btnValue' onClick={this.onClickAddItem}>add</button>
               </div>
 
-              <div>{this.state.inputValue}</div>
-
-              <ul>
-                  {this.state.list.map((item, id) => {
-                      return (<TodoListItem key={id} val={item} />);
-                  })}
+              <ul className="todoList">
+                  {todoItems}
               </ul>
           </div>
         );
